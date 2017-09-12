d9742b77f1e27cbeb6aebba843a8505b2dd3d919
Alecyrus <alecyrus@163.com>
2017-09-12 21:02:50 +0800

    Update retry_decorator.py

--- retry_decorator.py
+++ retry_decorator.py
@@ -8,7 +8,7 @@ def retry(ignore_exceptions, times=3, base_time=2, block=True, verbose=True):
     Usage:
         @retry(ignore_exceptions=[ValueError, ], times=3, base_time=2, block=True, debug=True)
 
-    :param ignore_exceptions(list): When the exceptions ares catched, the function decoratored will be retried at a
+    :param ignore_exceptions(list): When the exceptions are catched, the function decoratored will be retried at a
                                     later time. The default is None.
     :param times(integer): The max retry times. The default is 3.
     :param base_time(integer): Before the function decoratored reteies, it will be sleeped for a calulated time(SLEEP_TIME).
